921190a72b90f1c6b2231d733c8ed058240cc65a
shigehitonaito <shigehito.7110.330@gmail.com>
2020-06-04 09:34:01 +0000

    終了試験デバック1回目

---
+++ app/controllers/book_comments_controller.rb
@@ -0,0 +1,20 @@
+class BookCommentsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @comment = current_user.book_comments.new(comment_params)
+    @comment.book_id = params[:book_id].to_i
+    @comment.save
+  end
+
+  def destroy
+    @comment = BookComment.find(params[:id])
+    if @comment.user_id == current_user.id
+      @comment.destroy
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:book_comment).permit(:body)
+  end
+end
